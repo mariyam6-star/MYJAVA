15b5b7c2cc377029f94da5500d83c78265b893ee
mariyam y sidat <mariyamsidat8899@gmail.com>
2025-08-13 12:53:46 +0530

    widecasting

---
+++ widecast.java
@@ -0,0 +1,10 @@
+public class widecast{
+    public static void main(String []args)
+    {
+        int a=20;
+        double result=a;
+        System.out.println("wide type casting :");
+        System.out.println("a="+a);
+        System.out.println("result="+result);
+    }
+}
