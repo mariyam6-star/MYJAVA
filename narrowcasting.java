5651f6c5c4833dd8cc1bc98ca4f3cc60c3ba3fd6
mariyam y sidat <mariyamsidat8899@gmail.com>
2025-08-13 12:56:01 +0530

    narrowcasting

---
+++ narrowcasting.java
@@ -0,0 +1,10 @@
+public class narrowcasting{
+    public static void main(String []args)
+    {
+        double a=3.14;
+        double result=(int)a;
+        System.out.println("wide type casting :");
+        System.out.println("a="+a);
+        System.out.println("result="+result);
+    }
+}
